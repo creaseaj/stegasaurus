fa618980511ed93c56e138ddf2b5129a7c4c070d
Adam <creaseaj@gmail.com>
2022-10-17 09:27:34 +0100

    fix docker container

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: "https://3a34761f146a4bfdb1445b4eecf370bd@o4503981942243328.ingest.sentry.io/4503981943881728",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
