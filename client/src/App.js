89c74e2b43b7cf7eb28ee6f0ce86bf285d5c655f
Adam <creaseaj@gmail.com>
2022-10-18 13:51:37 +0100

    Fix react to work with docker

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        button!!
-        <button onClick={methodDoesNotExist}>Break the world</button>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className='gap-[30px] mt-[35px] h-[550px] flex'>
+        <div className='bg-[#1E293B] w-full max-w-[220px] h-full rounded-r-[10px]'>
+          Bookmarks
+        </div>
+        <div className='bg-slate-200 w-full h-full rounded-[10px]'>
+          Image!
+        </div>
+        <div className='bg-[#1E293B] w-full max-w-[220px] h-full rounded-l-[10px]'>
+          Bookmarks
+        </div>
+      </div>
+    </>
   );
 }
 
