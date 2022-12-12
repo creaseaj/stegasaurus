8a826504b5a89049ece8df15f48bcb1bcdb1c5f4
Adam Crease <creaseaj@gmail.com>
2022-12-12 13:02:06 +0000

    Better image mitm and gitignore

--- mitmproxy/scripts/addon.py
+++ mitmproxy/scripts/addon.py
@@ -1,12 +1,18 @@
 from mitmproxy import http
+import os
 
 
 def response(flow: http.HTTPFlow):
-    if flow.response.headers.get("content-type", "").startswith("image"):
-        img = open("test.jpeg", "rb").read()
-        open("images/" + flow.request.path.split('/')
-             [-1], "wb").write(flow.response.content)
+    if flow.response.headers.get("content-type", "").endswith(('jpg', 'jpeg')):
+        domain, port = flow.server_conn.address
+
+        if not os.path.exists("/home/images/" + domain):
+            os.mkdir("/home/images/" + domain)
+
+        if not os.path.exists("/home/images/" + domain + "/" + str(port)):
+            os.mkdir("/home/images/" + domain + "/" + str(port))
+        filename = flow.request.path.split('/')[-1].split('?')[0]
+        open("/home/images/" + domain + "/" + str(port) + "/" +
+             filename, "wb").write(flow.response.content)
         oldimg = flow.response.content
-        print('title:', flow.request.path.split('/')[-1])
-        # flow.response.content = img
         flow.response.headers["content-type"] = "image/jpeg"
