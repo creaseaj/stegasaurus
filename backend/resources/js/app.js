7b5223fc44e4ac6aefbc327752915b0b7acc35e4
creaseaj <creaseaj@gmail.com>
2023-02-13 16:40:56 +0000

    persist grid / list choice

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,5 +1,17 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import persist from '@alpinejs/persist';
+
+Alpine.plugin(persist)
+
+Alpine.store('isGrid', {
+    on: true,
+ 
+    toggle() {
+        this.on = ! this.on
+    }
+})
+ 
 
 Alpine.start();
